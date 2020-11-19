6836b95be935eb26912f9e57f671cdb4d5802816
ucakmak02 <u.s.cakmak02@gmail.com>
2020-11-19 21:38:51 +0100

    View

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 /* Import Style */
 import { layoutStyle } from '../components/Layout/theme'
 
 export default function DetailPage() {
-    const { key } = useParams();
+
     const styles = layoutStyle();
     return (
         <div className={styles.container}>
